9e3322f8f3db6aabdbc52d2971b8b493f0327825
Santiago Arango Gutierrez <santiago.arango@arquitectosdenegocios.com>
2021-09-21 18:26:43 -0500

    Fix add slider and safeguard sections

--- src/app/shop/shop.module.ts
+++ src/app/shop/shop.module.ts
@@ -3,16 +3,22 @@ import {CommonModule} from '@angular/common';
 import {HomeComponent} from './pages/home/home.component';
 import {ShopRoutingModule} from './shop-routing.module';
 import {SharedModule} from '../shared/shared.module';
+import {CarouselComponent} from './pages/carousel/carousel.component';
+import { BestSellerComponent } from './pages/best-seller/best-seller.component';
+import {TranslocoRootModule} from '../transloco-root.module';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    CarouselComponent,
+    BestSellerComponent
   ],
   imports: [
     CommonModule,
     ShopRoutingModule,
-    SharedModule
+    SharedModule,
+    TranslocoRootModule
   ]
 })
 export class ShopModule {
